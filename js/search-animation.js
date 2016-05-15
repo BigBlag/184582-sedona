b2a46ba0c73d22a37bb93d42418bb93f0c3ac3db
BigBlag <unholy6661313@gmail.com>
2016-05-15 20:53:18 +0300

    Исправил недочёты

--- js/search-animation.js
+++ js/search-animation.js
@@ -1,14 +1,14 @@
 var link = document.querySelector(".search-title");
 var searchForm = document.querySelector(".search-form");
-var date = searchForm.querySelector("#check-in");
+var date = searchForm.querySelector("#day-in");
 link.addEventListener("click", function(event) {
-    event.preventDefault();
-    if (searchForm.classList.contains("search-form-visible")) {
-        searchForm.classList.add("search-form-invisible");
-        searchForm.classList.remove("search-form-visible");
-    } else {
-        searchForm.classList.add("search-form-visible");
-        searchForm.classList.remove("search-form-invisible");
-    };
-    date.focus();
+event.preventDefault();
+if (searchForm.classList.contains("search-form-visible")) {
+    searchForm.classList.add("search-form-invisible");
+    searchForm.classList.remove("search-form-visible");
+} else {
+    searchForm.classList.add("search-form-visible");
+    searchForm.classList.remove("search-form-invisible");
+};
+date.focus();
 });
